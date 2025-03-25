68efe43f32b0711181ec256723feb712728a8ca3
R Chaganti <rajesh.chaganti@polygon.technology>
2025-03-25 22:57:38 +0400

    initialize databse helper

--- packages/consumer/src/index.ts
+++ packages/consumer/src/index.ts
@@ -1,10 +1,12 @@
 import { Logger } from "bridge-hub-commons/helpers/logger";
 import { JSONRPCClient } from "bridge-hub-commons/helpers/json_rpc_client";
 import dotenv from "dotenv";
+import { DatabaseClient } from "bridge-hub-commons/helpers/database";
 
 dotenv.config();
 
 let jsonRPCClient: JSONRPCClient;
+let database: DatabaseClient;
 
 async function start(): Promise<void> {
     try {
@@ -19,6 +21,10 @@ async function start(): Promise<void> {
         });
 
         jsonRPCClient = new JSONRPCClient(process.env.BRIDGE_SERVICE_URL ?? "");
+        database = new DatabaseClient(
+            process.env.GOOGLE_CLOUD_PROJECT_ID ?? "",
+            process.env.FIRESTORE_DATABASE_ID ?? ""
+        );
 
         Logger.info(
             `Consumer for network id ${process.env.NETWORK_ID} started`
