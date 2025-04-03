a291838864fbbc7bed3c6aecdd2118e119b93c1f
R Chaganti <rajesh.chaganti@polygon.technology>
2025-04-04 03:38:28 +0400

    fix: semantic-release config and workflow

--- packages/consumer/release.config.js
+++ packages/consumer/release.config.js
@@ -1,21 +1,9 @@
 const _default = {
-    branches: ["release"],
+    branches: ["main"],
     tagFormat: "bridge-hub-commons-v${version}",
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        [
-            "@semantic-release/git",
-            {
-                assets: [
-                    "packages/commons/package.json",
-                    "packages/commons/CHANGELOG.md",
-                ],
-                message:
-                    "chore(bridge-hub-commons release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-            },
-        ],
         "@semantic-release/github",
     ],
 };
