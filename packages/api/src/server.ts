1f02ab4cc4eac099ccdfce09a83b922c94b46876
R Chaganti <rajesh.chaganti@polygon.technology>
2025-04-04 16:17:45 +0400

    chore: initialise database client in api

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -2,15 +2,37 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import router from "./routes";
+import { Logger } from "bridge-hub-commons/helpers/logger";
+import { DatabaseClient } from "bridge-hub-commons/helpers/database";
 
 const app = new Hono();
+let database: DatabaseClient;
 
-// Middlewares
-app.use("*", logger()); // Logs all requests
-app.use("*", cors()); // Enables CORS for all routes
+async function serve(): Promise<void> {
+    Logger.create({
+        sentry: {
+            dsn: process.env.SENTRY_DSN,
+            level: "error",
+        },
+        console: {
+            level: "info",
+        },
+    });
 
-// Register routes
-app.route("/", router);
+    database = new DatabaseClient(
+        process.env.GOOGLE_CLOUD_PROJECT_ID ?? "",
+        process.env.FIRESTORE_DATABASE_ID ?? ""
+    );
+
+    // Middlewares
+    app.use("*", logger()); // Logs all requests
+    app.use("*", cors()); // Enables CORS for all routes
+
+    // Register routes
+    app.route("/", router);
+}
+
+serve();
 
 export default {
     port: process.env.PORT || 3000,
