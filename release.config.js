56512281bde04a1b87f0bde809272fa573ae01e9
Saurabh Shrihar <sshrihar@polygon.technology>
2025-04-04 14:39:35 +0400

    fix(NO-JIRA): update release workflow config for semantic versioning

---
+++ release.config.js
@@ -0,0 +1,10 @@
+const _default = {
+    branches: ["main"],
+    tagFormat: "v${version}",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+    ],
+};
+export { _default as default };
