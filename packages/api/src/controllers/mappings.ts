118168bc98b2d38fd20b47dbaf2ee376bc3b1f08
py-zoid <rajesh.chaganti@polygon.technology>
2025-09-19 13:29:37 +0400

    fix: refactor api code to move query building logic to services

--- packages/api/src/controllers/mappings.ts
+++ packages/api/src/controllers/mappings.ts
@@ -1,55 +1,21 @@
 import type { Context } from "hono";
 import { MappingsService } from "../services/mappings";
-import {
-	handleResponse,
-	type IQueryFilterOperationParams,
-	type IQueryOrFilterParams,
-} from "@polygonlabs/servercore";
+import { handleResponse } from "@polygonlabs/servercore";
 import { getResponseContext } from "../middlewares/response_context";
 
 export const getMappings = async (c: Context) => {
 	const query = c.get("validatedQuery");
 	const { network } = c.get("validatedParams");
 
-	// Create query params for db request
-	const queryParams: IQueryFilterOperationParams[] = [];
-
-	if (query.originTokenAddress) {
-		queryParams.push({
-			field: "originTokenAddress",
-			operator: "==",
-			value: query.originTokenAddress,
-		});
-	}
-	if (query.wrappedTokenAddress) {
-		queryParams.push({
-			field: "wrappedTokenAddress",
-			operator: "==",
-			value: query.wrappedTokenAddress,
-		});
-	}
-	if (query.originNetworkIds) {
-		queryParams.push({
-			field: "originTokenNetwork",
-			operator: "in",
-			value: query.originNetworkIds,
-		});
-	}
-	if (query.wrappedNetworkIds) {
-		queryParams.push({
-			field: "wrappedTokenNetwork",
-			operator: "in",
-			value: query.wrappedNetworkIds,
-		});
-	}
-
-	const mappings = await MappingsService.getMappings(
+	const mappings = await MappingsService.getMappings({
 		network,
-		queryParams,
-		undefined,
-		query.limit,
-		query.startAfter
-	);
+		originNetworkIds: query.originNetworkIds,
+		originTokenAddress: query.originTokenAddress,
+		wrappedNetworkIds: query.wrappedNetworkIds,
+		wrappedTokenAddress: query.wrappedTokenAddress,
+		limit: query.limit,
+		startAfter: query.startAfter,
+	});
 
 	return handleResponse(getResponseContext(c), mappings.documents, {
 		total: mappings.totalDocumentsCount || 0,
@@ -65,48 +31,10 @@ export const getMappingsByToken = async (c: Context) => {
 	const query = c.get("validatedQuery");
 	const { tokenNetwork, tokenAddress, network } = c.get("validatedParams");
 
-	// Create query params for db request
-	const queryParams: IQueryOrFilterParams[] = [];
-
-	if (tokenAddress) {
-		queryParams.push({
-			or: [
-				{
-					field: "originTokenAddress",
-					operator: "==",
-					value: tokenAddress,
-				},
-				{
-					field: "wrappedTokenAddress",
-					operator: "==",
-					value: tokenAddress,
-				},
-			],
-		});
-	}
-	if (tokenNetwork) {
-		queryParams.push({
-			or: [
-				{
-					field: "originTokenNetwork",
-					operator: "==",
-					value: tokenNetwork,
-				},
-				{
-					field: "wrappedTokenNetwork",
-					operator: "==",
-					value: tokenNetwork,
-				},
-			],
-		});
-	}
-
-	const mappings = await MappingsService.getMappings(
-		network,
-		undefined,
-		queryParams,
-		query.limit,
-		query.startAfter
+	const mappings = await MappingsService.getMappingsByToken(
+		tokenAddress,
+		tokenNetwork,
+		network
 	);
 
 	return handleResponse(getResponseContext(c), mappings.documents, {
