f95cf23c4dee9944ff4383d90b13abfa273d98dd
Nitin Mittal <nitinmittal44066@gmail.com>
2025-10-13 19:27:46 +0530

    fix: auto claim health check apis

--- packages/api/src/services/health_check.ts
+++ packages/api/src/services/health_check.ts
@@ -3,21 +3,16 @@ import { TransactionService } from "./transactions";
 import { createPublicClient, http } from "viem";
 
 let chainConfig: Map<string, Map<number, string>>;
-let autoclaimAddress: Map<string, string>;
+const autoclaimAddress = "0x616b3Af96437f69B31D03EBbD64Bbc967CE80361";
 
 export class HealthCheckService {
 	static initializeHealthCheckService(
 		chainConfigParam: Map<string, Map<number, string>> = new Map([
 			["mainnet", new Map([])],
 			["testnet", new Map([])],
-		]),
-		autoclaimAddressParam: Map<string, string> = new Map([
-			["mainnet", "0x616b3Af96437f69B31D03EBbD64Bbc967CE80361"],
-			["testnet", "0xC2517e892B487A3d6F37d81F70cBC9C7ab4C509B"],
 		])
 	) {
 		chainConfig = chainConfigParam;
-		autoclaimAddress = autoclaimAddressParam;
 	}
 
 	static async checkForAutoClaim(
@@ -33,10 +28,11 @@ export class HealthCheckService {
 		});
 
 		if (transactions?.documents?.length) {
+			const filteredTxns = transactions.documents.filter(
+				(obj) => obj.leafType === "ASSET"
+			);
 			if (
-				transactions.documents[transactions.documents.length - 1]
-					.timestamp *
-					1000 <
+				filteredTxns[filteredTxns.length - 1].timestamp * 1000 <
 				Date.now() - 60 * 60 * 1000
 			) {
 				throw new ApiError(
@@ -46,8 +42,7 @@ export class HealthCheckService {
 		}
 
 		const rpc = chainConfig.get(network)?.get(parseInt(networkId, 10));
-		const autoClaimAddress = autoclaimAddress.get(network);
-		if (!rpc || !autoClaimAddress) {
+		if (!rpc) {
 			throw new ApiError(
 				`RPC not configured for network ${network} ${networkId}`
 			);
@@ -57,7 +52,7 @@ export class HealthCheckService {
 			transport: http(rpc),
 		});
 		const balance = await client.getBalance({
-			address: autoClaimAddress as `0x${string}`,
+			address: autoclaimAddress as `0x${string}`,
 		});
 		if (balance < BigInt("10000000000000000")) {
 			throw new ApiError(
