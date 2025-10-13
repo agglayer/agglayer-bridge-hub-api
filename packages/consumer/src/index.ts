7957634d7839c47c1b698bfdb174b4c0700f3370
Nitin Mittal <nitinmittal44066@gmail.com>
2025-10-13 14:39:38 +0530

    feat: resyncing health check flag

--- packages/consumer/src/index.ts
+++ packages/consumer/src/index.ts
@@ -1,10 +1,10 @@
 import {
-	// ApiError,
+	ApiError,
 	Logger,
 	setupHealthCheckServer,
 } from "@polygonlabs/servercore";
-// import axios from "axios";
-// import { createPublicClient, http } from "viem";
+import axios from "axios";
+import { createPublicClient, http } from "viem";
 
 // Initialize the logger globally
 Logger.create({
@@ -26,14 +26,14 @@ import MetadataService from "./services/metadata";
 import MetadataMapper from "./mappers/metadata";
 import { DatabaseClient } from "@polygonlabs/servercore-firestore";
 import { ClaimReadinessConsumer } from "./claim_readiness_consumer";
-// import bridgeAbi from "./interfaces/PolygonZkEVMBridge";
+import bridgeAbi from "./interfaces/PolygonZkEVMBridge";
 
 let database: DatabaseClient;
 
-// const bridgeAddress = new Map([
-// 	["mainnet", "0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe"],
-// 	["testnet", "0x528e26b25a34a4A5d0dbDa1d57D318153d2ED582"],
-// ]);
+const bridgeAddress = new Map([
+	["mainnet", "0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe"],
+	["testnet", "0x528e26b25a34a4A5d0dbDa1d57D318153d2ED582"],
+]);
 
 async function start(): Promise<void> {
 	try {
@@ -133,139 +133,143 @@ async function start(): Promise<void> {
 			transactionService
 		);
 
-		// const client = createPublicClient({
-		// 	transport: http(process.env.RPC_URL || ""),
-		// });
+		const client = createPublicClient({
+			transport: http(process.env.RPC_URL || ""),
+		});
 
 		setupHealthCheckServer(
 			[],
-			Number(process.env.HEALTH_CHECK_PORT || "3001")
-			// async () => {
-			// 	try {
-			// 		const [
-			// 			depositCount,
-			// 			bridges,
-			// 			mappings,
-			// 			claims,
-			// 			latestBridgeFromDB,
-			// 		] = await Promise.all([
-			// 			client.readContract({
-			// 				address: (process.env.BRIDGE_CONTRACT_ADDRESS ||
-			// 					bridgeAddress.get(
-			// 						process.env.NETWORK || "mainnet"
-			// 					)) as `0x${string}`,
-			// 				abi: bridgeAbi,
-			// 				functionName: "depositCount",
-			// 			}),
-			// 			axios.get(
-			// 				`${process.env.BRIDGE_SERVICE_URL}/bridges?network_id=${process.env.NETWORK_ID}`
-			// 			),
-			// 			axios.get(
-			// 				`${process.env.BRIDGE_SERVICE_URL}/token-mappings?network_id=${process.env.NETWORK_ID}`
-			// 			),
-			// 			axios.get(
-			// 				`${process.env.BRIDGE_SERVICE_URL}/claims?network_id=${process.env.NETWORK_ID}`
-			// 			),
-			// 			transactionService.getLatestBridgeTransactions(
-			// 				Number(process.env.NETWORK_ID)
-			// 			),
-			// 		]);
+			Number(process.env.HEALTH_CHECK_PORT || "3001"),
+			async () => {
+				try {
+					if (process.env.RESYNCING === "true") {
+						return true;
+					}
 
-			// 		const depositCnt =
-			// 			Number((depositCount as bigint).toString()) - 1;
-			// 		const latestBridge = bridges?.data?.bridges?.[0] || null;
-			// 		const latestMapping =
-			// 			mappings?.data?.token_mappings?.[0] || null;
-			// 		const latestClaims = claims?.data?.claims?.[0] || null;
-			// 		const latestBridgeInDB = latestBridgeFromDB[0] || null;
+					const [
+						depositCount,
+						bridges,
+						mappings,
+						claims,
+						latestBridgeFromDB,
+					] = await Promise.all([
+						client.readContract({
+							address: (process.env.BRIDGE_CONTRACT_ADDRESS ||
+								bridgeAddress.get(
+									process.env.NETWORK || "mainnet"
+								)) as `0x${string}`,
+							abi: bridgeAbi,
+							functionName: "depositCount",
+						}),
+						axios.get(
+							`${process.env.BRIDGE_SERVICE_URL}/bridges?network_id=${process.env.NETWORK_ID}`
+						),
+						axios.get(
+							`${process.env.BRIDGE_SERVICE_URL}/token-mappings?network_id=${process.env.NETWORK_ID}`
+						),
+						axios.get(
+							`${process.env.BRIDGE_SERVICE_URL}/claims?network_id=${process.env.NETWORK_ID}`
+						),
+						transactionService.getLatestBridgeTransactions(
+							Number(process.env.NETWORK_ID)
+						),
+					]);
 
-			// 		if (!latestBridge) {
-			// 			throw new ApiError("No bridges found from Aggkit API");
-			// 		}
+					const depositCnt =
+						Number((depositCount as bigint).toString()) - 1;
+					const latestBridge = bridges?.data?.bridges?.[0] || null;
+					const latestMapping =
+						mappings?.data?.token_mappings?.[0] || null;
+					const latestClaims = claims?.data?.claims?.[0] || null;
+					const latestBridgeInDB = latestBridgeFromDB[0] || null;
 
-			// 		if (!latestClaims) {
-			// 			throw new ApiError("No claims found from Aggkit API");
-			// 		}
+					if (!latestBridge) {
+						throw new ApiError("No bridges found from Aggkit API");
+					}
 
-			// 		if (!latestMapping) {
-			// 			throw new ApiError(
-			// 				"No token mappings found from Aggkit API"
-			// 			);
-			// 		}
+					if (!latestClaims) {
+						throw new ApiError("No claims found from Aggkit API");
+					}
 
-			// 		if (!latestBridgeInDB || !latestBridgeInDB.timestamp) {
-			// 			throw new ApiError(
-			// 				"No bridges saved in DB but bridges found in Aggkit API"
-			// 			);
-			// 		}
+					if (!latestMapping) {
+						throw new ApiError(
+							"No token mappings found from Aggkit API"
+						);
+					}
 
-			// 		// if deposit count from contract is larger then saved
-			// 		if (depositCnt !== latestBridge.deposit_count) {
-			// 			throw new ApiError(`Aggkit not in sync`);
-			// 		}
+					if (!latestBridgeInDB || !latestBridgeInDB.timestamp) {
+						throw new ApiError(
+							"No bridges saved in DB but bridges found in Aggkit API"
+						);
+					}
 
-			// 		// if deposit count from contract is larger then saved
-			// 		if (
-			// 			latestBridge.deposit_count >
-			// 				latestBridgeInDB.depositCount &&
-			// 			Date.now() -
-			// 				new Date(latestBridge.timestamp * 1000).getTime() >
-			// 				5 * 60 * 1000
-			// 		) {
-			// 			throw new ApiError(
-			// 				`Deposits in DB not in sync. Aggkit: ${latestBridge.deposit_count}, DB: ${latestBridgeInDB.depositCount}`
-			// 			);
-			// 		}
+					// if deposit count from contract is larger then saved
+					if (depositCnt !== latestBridge.deposit_count) {
+						throw new ApiError(`Aggkit not in sync`);
+					}
 
-			// 		const [mappingsFromDb, claimFromDb] = await Promise.all([
-			// 			tokenMappingsService.getLatestTokenMapping(
-			// 				latestMapping.tx_hash.toLowerCase(),
-			// 				latestMapping.block_num
-			// 			),
-			// 			transactionService.getClaim(
-			// 				latestClaims.tx_hash.toLowerCase()
-			// 			),
-			// 		]);
+					// if deposit count from contract is larger then saved
+					if (
+						latestBridge.deposit_count >
+							latestBridgeInDB.depositCount &&
+						Date.now() -
+							new Date(latestBridge.timestamp * 1000).getTime() >
+							5 * 60 * 1000
+					) {
+						throw new ApiError(
+							`Deposits in DB not in sync. Aggkit: ${latestBridge.deposit_count}, DB: ${latestBridgeInDB.depositCount}`
+						);
+					}
 
-			// 		if (
-			// 			(!mappingsFromDb || mappingsFromDb.length === 0) &&
-			// 			Date.now() -
-			// 				new Date(
-			// 					latestMapping.block_timestamp * 1000
-			// 				).getTime() >
-			// 				5 * 60 * 1000
-			// 		) {
-			// 			throw new ApiError(
-			// 				`Mappings in DB not in sync. Aggkit: ${latestMapping.tx_hash}`
-			// 			);
-			// 		}
+					const [mappingsFromDb, claimFromDb] = await Promise.all([
+						tokenMappingsService.getLatestTokenMapping(
+							latestMapping.tx_hash.toLowerCase(),
+							latestMapping.block_num
+						),
+						transactionService.getClaim(
+							latestClaims.tx_hash.toLowerCase()
+						),
+					]);
 
-			// 		if (
-			// 			(!claimFromDb || claimFromDb.length === 0) &&
-			// 			Date.now() -
-			// 				new Date(
-			// 					latestClaims.block_timestamp * 1000
-			// 				).getTime() >
-			// 				5 * 60 * 1000
-			// 		) {
-			// 			throw new ApiError(
-			// 				`Claims in DB not in sync. Aggkit: ${latestClaims.tx_hash}`
-			// 			);
-			// 		}
+					if (
+						(!mappingsFromDb || mappingsFromDb.length === 0) &&
+						Date.now() -
+							new Date(
+								latestMapping.block_timestamp * 1000
+							).getTime() >
+							5 * 60 * 1000
+					) {
+						throw new ApiError(
+							`Mappings in DB not in sync. Aggkit: ${latestMapping.tx_hash}`
+						);
+					}
 
-			// 		return true;
-			// 	} catch (error: any) {
-			// 		Logger.info({
-			// 			location: "bridge_api_consumer_index",
-			// 			function: "setupHealthCheckServer",
-			// 			status: `ERROR encountered on health check`,
-			// 			error: error,
-			// 		});
-			// 		throw error instanceof ApiError
-			// 			? error
-			// 			: new ApiError(error.message);
-			// 	}
-			// }
+					if (
+						(!claimFromDb || claimFromDb.length === 0) &&
+						Date.now() -
+							new Date(
+								latestClaims.block_timestamp * 1000
+							).getTime() >
+							5 * 60 * 1000
+					) {
+						throw new ApiError(
+							`Claims in DB not in sync. Aggkit: ${latestClaims.tx_hash}`
+						);
+					}
+
+					return true;
+				} catch (error: any) {
+					Logger.info({
+						location: "bridge_api_consumer_index",
+						function: "setupHealthCheckServer",
+						status: `ERROR encountered on health check`,
+						error: error,
+					});
+					throw error instanceof ApiError
+						? error
+						: new ApiError(error.message);
+				}
+			}
 		);
 		await bridgeAPIConsumer.start();
 		await claimReadinessConsumer.start();
