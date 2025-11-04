b265f1adf4575bc191e8964d70afbbe050c63d5f
py-zoid <rajesh.chaganti@polygon.technology>
2025-11-04 10:55:26 +0700

    fix: make leafIndex param optional for proof generation

--- packages/api/src/controllers/proof.ts
+++ packages/api/src/controllers/proof.ts
@@ -1,11 +1,14 @@
 import type { Context } from "hono";
 import type { ClaimProofQuery } from "../schemas";
 import { ProofService } from "../services/proof";
+import { TransactionService } from "../services/transactions";
 import {
 	ApiError,
+	BadRequestError,
 	ExternalDependencyError,
 	handleError,
 	handleResponse,
+	NotFoundError,
 } from "@polygonlabs/servercore";
 import { getResponseContext } from "../middlewares/response_context";
 
@@ -13,6 +16,41 @@ export const getProof = async (c: Context) => {
 	try {
 		const validatedQuery: ClaimProofQuery = c.get("validatedQuery");
 		const { network } = c.get("validatedParams");
+
+		if (!validatedQuery.leafIndex) {
+			const docId = TransactionService.generateDocId(
+				validatedQuery.depositCount,
+				validatedQuery.sourceNetworkId
+			);
+
+			const transaction =
+				await TransactionService.getTransactionByDepositCount(
+					network,
+					docId
+				);
+
+			if (!transaction) {
+				throw new NotFoundError(
+					"Transaction not found for the given deposit count and source networkId",
+					undefined,
+					undefined,
+					{
+						network: network,
+						sourceNetwork: validatedQuery.sourceNetworkId,
+						depositCount: validatedQuery.depositCount,
+					}
+				);
+			} else if (
+				transaction.status === "READY_TO_CLAIM" &&
+				transaction.leafIndexForProof
+			) {
+				validatedQuery.leafIndex = transaction.leafIndexForProof;
+			} else if (transaction.status === "CLAIMED") {
+				throw new BadRequestError("Transaction already claimed");
+			} else {
+				throw new BadRequestError("Transaction not ready to claim");
+			}
+		}
 
 		const proof = await ProofService.getProof(
 			network,
