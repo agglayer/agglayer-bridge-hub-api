7105a1a79334523996689746cf07b5254858d03b
py-zoid <rajesh.chaganti@polygon.technology>
2026-01-23 15:43:45 +0400

    fix: devnet bridge address

--- packages/consumer/src/config.ts
+++ packages/consumer/src/config.ts
@@ -3,7 +3,7 @@
 const BRIDGE_ADDRESSES = new Map([
 	["mainnet", "0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe"],
 	["testnet", "0x528e26b25a34a4A5d0dbDa1d57D318153d2ED582"],
-	["testnet", "0x1348947e282138d8f377b467F7D9c2EB0F335d1f"],
+	["devnet", "0x1348947e282138d8f377b467F7D9c2EB0F335d1f"],
 ]);
 
 const COLLECTIONS_CONFIG = new Map([
